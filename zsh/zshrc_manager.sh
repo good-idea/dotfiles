b7437970bd13e68c89e6848632a64956374ba26a
Joseph <joseph@good-idea.studio>
2022-01-16 11:59:51 -0700

    fix tmux launch command

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,5 +1,5 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
-if [ "$TMUX" = "" ]; then exec tmux; fi
+if [ "$TMUX" = "" ]; then exec tmux attach-session; fi
 
 source ~/.dotfiles/zsh/zshrc.sh
