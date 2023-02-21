9086359f39c62407f1713ec838fb1309f9125cf0
Joseph <joseph@good-idea.studio>
2023-02-20 17:15:15 -0800

    enable eslint_d

--- vim/lua/config/lsp_config.lua
+++ vim/lua/config/lsp_config.lua
@@ -73,17 +73,17 @@ vim.cmd [[
 
 -- -- null-ls
 -- -- configure null-ls to work with eslint_d and prettier
--- local null_ls = require("null-ls")
+local null_ls = require("null-ls")
 
--- null_ls.setup(
---   {
---     sources = {
---       null_ls.builtins.diagnostics.eslint.with({}),
---       null_ls.builtins.code_actions.eslint,
---       -- null_ls.builtins.diagnostics.eslint_d,
---       -- null_ls.builtins.code_actions.eslint_d,
---       null_ls.builtins.formatting.prettier
---     },
---     on_attach = on_attach
---   }
--- )
+null_ls.setup(
+  {
+    sources = {
+      -- null_ls.builtins.diagnostics.eslint.with({}),
+      -- null_ls.builtins.code_actions.eslint,
+      null_ls.builtins.diagnostics.eslint_d,
+      null_ls.builtins.code_actions.eslint_d,
+      null_ls.builtins.formatting.prettier
+    },
+    on_attach = on_attach
+  }
+)
