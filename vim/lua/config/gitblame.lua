c7f46338b81cfc822a775191fb2e6aea6c0df0bc
Joseph <joseph@good-idea.studio>
2024-08-21 15:46:40 -0700

    fix: update git blame format

--- vim/lua/config/gitblame.lua
+++ vim/lua/config/gitblame.lua
@@ -1,5 +1,6 @@
 vim.cmd [[
-  let g:gitblame_message_template = '<sha> • <summary> • <author>'
+  let g:gitblame_message_template = '<sha> • <date> • <summary> • <author>'
   " Disable in-line blame
   let g:gitblame_display_virtual_text = 0
+  let g:gitblame_date_format = "%r"
 ]]
