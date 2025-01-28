8dea9705c806db742b9b0f5c81e793b41517f7c1
Joseph <joseph@good-idea.studio>
2025-01-28 13:50:31 -0800

    tweak oil keymaps

--- vim/lua/config/oil.lua
+++ vim/lua/config/oil.lua
@@ -1,3 +1,15 @@
-require("oil").setup()
+require("oil").setup(
+  {
+    keymaps = {
+      ["?"] = "actions.show_help",
+      ["v"] = {"actions.select", opts = {vertical = true}, desc = "Open the entry in a vertical split"},
+      ["s"] = {"actions.select", opts = {horizontal = true}, desc = "Open the entry in a horizontal split"},
+      ["t"] = {"actions.select", opts = {tab = true}, desc = "Open the entry in new tab"}
+    },
+    view_options = {
+      show_hidden = true
+    }
+  }
+)
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", {desc = "Open parent directory"})
